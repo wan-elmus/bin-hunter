909213b0ec933efe93f7816ae3d3c9e631e294e3
wan-elmus <elmuswangudi@gmail.com>
2023-01-23 22:11:54 +0300

    Full implementation of script requirements

--- searchbin.sh
+++ searchbin.sh
@@ -5,99 +5,109 @@
 #validate the input arguments
 validate_args() {
 
-#Check if the correct number of arguments are provided
-if [ $# -ne 2 ]; then
-tput setaf 1
-echo "Please provide two arguments: option and argument. Exiting..."
-tput sgr0
-exit 1
-fi
+    #Check if the correct number of arguments are provided
+    if [ $# -ne 2 ]; then
+        tput setaf 1
+        echo "Invalid flag or missing argument error - exiting.."
+        tput sgr0
+    exit 1
+    fi
 
-#Store option and argument in variables
-option=$1
-argument=$2
+    #Store option and argument in variables
+    option=$(echo $1 | tr '[:upper:]' '[:lower:]')
+    argument=$(echo $2 | tr '[:upper:]' '[:lower:]')
 
-case $option in
-# Validate -z option
-"-z")
-if [[ $argument != "ASC" && $argument != "DSC" && $argument != "SHL" && $argument != "SLH" ]]; then
-tput setaf 1
-echo "Invalid argument for -z option. Exiting..."
-tput sgr0
-exit 1
-fi
-;;
-# Validate -s option
-"-s")
-if [[ -z $argument ]]; then
-tput setaf 1
-echo "Invalid argument for -s option. Exiting..."
-tput sgr0
-exit 1
-fi
-;;
-# Validate -b option
-"-b")
-operator=${argument%,*}
-bytes=${argument#*,}
-if [[ -z $operator || -z $bytes ]]; then
-tput setaf 1
-echo "Invalid argument for -b option. Exiting..."
-tput sgr0
-exit 1
-fi
-case $operator in
-"GT") operator=">";;
-"LT") operator="<";;
-"LE") operator="<=";;
-"GE") operator=">=";;
-"EQ") operator="==";;
-"NE") operator="!=";;
-*)
-tput setaf 1
-echo "Invalid operator for -b option. Exiting..."
-tput sgr0
-exit 1
-;;
-esac
-;;
-*)
-tput setaf 1
-echo "Invalid option. Exiting..."
-tput sgr0
-exit 1
-;;
-esac
+    case $option in
+    # Validate -z option
+    "-z")
+    if [[ $argument != "asc" && $argument != "dsc" && $argument != "shl" && $argument != "slh" ]]; then
+        tput setaf 1
+        echo "Invalid sort by argument. Exiting..."
+        tput sgr0
+        exit 1
+    fi
+    ;;
+    # Validate -s option
+    "-s")
+    if [[ -z $argument ]]; then
+        tput setaf 1
+        echo "Invalid sort by argument. Exiting..."
+        tput sgr0
+        exit 1
+    fi
+    ;;
+    # Validate -b option
+    "-b")
+    operator=$(echo ${argument%,*} | tr '[:upper:]' '[:lower:]')
+            bytes=${argument#*,}
+            if [[ -z $operator || -z $bytes ]]; then
+                tput setaf 1
+                echo "Invalid sort by argument. Exiting..."
+                tput sgr0
+                exit 1
+            fi
+            case $operator in
+                "gt") operator=">";;
+                "lt") operator="<";;
+                "le") operator="<=";;
+                "ge") operator=">=";;
+                "eq") operator="==";;
+                "ne") operator="!=";;
+                *)
+                tput setaf 1
+                echo "Invalid operator for -b option. Exiting..."
+                tput sgr0
+                exit 1
+                ;;
+            esac
+            ;;
+        *)
+            tput setaf 1
+            echo "Invalid option. Exiting..."
+            tput sgr0
+            exit 1
+            ;;
+    esac
 }
+# Function to print the header
+print_header() {
+    printf "%-20s %22s\n" "NAME" "SIZE"
+} 
 
 #Function to sort and display the output
 display_output() {
+    argument=$(echo $1 | tr '[:upper:]' '[:lower:]')
 
-#Get the list of files in /bin directory
-files=$(ls /bin)
+    #Get the list of files in /bin directory
+    files=$(ls /bin)
 
-#Sort the files based on the option provided
-case $1 in
-"ASC") files=$(echo "$files" | sort -f);;
-"DSC") files=$(echo "$files" | sort -rf);;
-"SHL") files=$(ls -S /bin);;
-"SLH") files=$(ls -Sr /bin);;
-esac
-
-#Display the files in columnar format
-echo "      Name                Size"
-echo "-------------------   -----------"
-for file in $files; do
-size=$(stat -c%s "/bin/$file")
-printf "%-20s %10d\n" $file $size
-done
+    #Sort the files based on the option provided
+    case $1 in
+    "asc" | "ASC") files=$(echo "$files" | sort -f);;
+    "dsc" | "DSC") files=$(echo "$files" | sort -rf);;
+    "shl" | "SHL") files=$(ls -S /bin);;
+    "slh" | "SLH") files=$(ls -Sr /bin);;
+    esac
+    print_header
+    #Display the files in columnar format
+    for file in $files; do
+        size=$(stat -c%s "/bin/$file")
+        if [[ $size -lt 1000 ]]; then
+                printf "%-20s %20db\n" $file $size
+        elif [[ $size -ge 1000000 ]]; then
+                size=$(echo "scale=2; $size/1000000" | bc)
+                printf "%-20s %20.2fMb\n" $file $size
+        else
+                size=$(echo "scale=2; $size/1000" | bc)
+                printf "%-20s %20.2fkb\n" $file  $size
+        fi
+    done
 }
 
-#filter the files based on the size
+#filter files based on the size
 filter_by_size() {
     operator=$1
     bytes=$2
-    # files=$(find /bin -type f -printf "%s %p\n" | awk '{if( $1 '"$operator $bytes"') print $2}')
     files=$(stat -c "%s %n" /bin/* | awk '{if($1 '"$operator $bytes"') print $2}')
 
     if [ -z "$files" ]; then
@@ -106,56 +116,83 @@ filter_by_size() {
     tput sgr0
     exit 0
     fi
-    echo "      Name                Size"
-    echo "-------------------   -----------"
+    #Display output for -b option
+    print_header
     for file in $files; do
-    size=$(stat -c%s "$file")
-    printf "%-20s %10d\n" $(basename $file) $size
+        size=$(stat -c%s "$file")
+        if [[ $size -lt 1000 ]]; then
+                printf "%-20s %20db\n" $(basename $file) $size
+        elif [[ $size -ge 1000000 ]]; then
+                size=$(echo "scale=2; $size/1000000" | bc)
+                printf "%-20s %20.2fMb\n" $(basename $file) $size
+        else
+                size=$(echo "scale=2; $size/1000" | bc)
+                printf "%-20s %20.2fkb\n" $(basename $file)  $size
+        fi
+    done
+}
+#list the utilities and commands in /bin directory alphabetically
+list_alphabetically() {
+    files=$(ls /bin | sort -f)
+    print_header
+    for file in $files; do
+        size=$(stat -c%s "/bin/$file")
+        if [[ $size -lt 1000 ]]; then
+                printf "%-20s %20dfb\n" $file $size
+        elif [[ $size -ge 1000000 ]]; then
+                size=$(echo "scale=2; $size/1000000" | bc)
+                printf "%-20s %20.2fmb\n" $file $size
+        else
+                size=$(echo "scale=2; $size/1000" | bc)
+                printf "%-20s %20.2fkb\n" $file  $size
+        fi
     done
 }
 
 #Main script
 if [ $# -eq 0 ]; then
-
 #If no argument is provided, display the full listing of /bin directory
-ls /bin
+    list_alphabetically
 else
-validate_args $1 $2
+    validate_args $1 $2
 
-option=$1
-argument=$2
+    option=$1
+    argument=$2
 
-case $option in
-"-z")
-  display_output $argument
-  ;;
-"-s")
-  files=$(ls /bin | grep -i $argument)
-  if [ -z "$files" ]; then
-  tput setaf 1
-    echo "No matches found"
-    tput sgr0
-    exit 0
-  fi
-  echo "      Name                Size"
-  echo "-------------------   -----------"
-for file in $files; do
-size=$(stat -c%s "/bin/$file")
-printf "%-20s %10d\n" $file $size
-done
-  ;;
-"-b")
-  filter_by_size $operator $bytes
+    case $option in
+    "-z")
+        display_output $argument
+        ;;
+    "-s")
+        files=$(ls /bin | grep -i $argument)
+    if [ -z "$files" ]; then
+        tput setaf 1
+        echo "No matches found"
+        tput sgr0
+        exit 0
+    fi
+        print_header
+        for file in $files; do
+            size=$(stat -c%s "/bin/$file")
+            if [[ $size -lt 1000 ]]; then
+                printf "%-20s %20d b\n" $file $size
+            elif [[ $size -ge 1000000 ]]; then
+                size=$(echo "scale=2; $size/1000000" | bc)
+                printf "%-20s %20.2fMb\n" $file $size
+            else
+                size=$(echo "scale=2; $size/1000" | bc)
+                printf "%-20s %20.2fkb\n" $file  $size
+            fi
+        done
+        ;;
+    "-b")
+        filter_by_size $operator $bytes
   ;;
 *)
-tput setaf 1
+  tput setaf 1
   echo "Invalid option. Exiting..."
   tput sgr0
   exit 1
   ;;
 esac
 fi
-
-
-
-
